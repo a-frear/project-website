10e446761d32bd41944097ab2d728ca9613d1d58
a_frear <amy.frear@gmail.com>
2020-11-14 01:41:03 -0500

    Added #phillyTweet

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ $('#videoLink2').magnificPopup({
   showCloseBtn: true,
   enableEscapeKey: true,
 });
+
+$('#tweetLink').magnificPopup({
+  type:'inline',
+  midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  closeBtnInside: false,
+  showCloseBtn: true,
+  enableEscapeKey: true,
+});
