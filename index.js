4971e4ccd5a16c11abbf5032fc8cf022f0ef64eb
a_frear <amy.frear@gmail.com>
2020-11-13 23:54:21 -0500

    Added js and secret videos

---
+++ index.js
@@ -0,0 +1,15 @@
+$('#videoLink').magnificPopup({
+  type:'inline',
+  midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  closeBtnInside: false,
+  showCloseBtn: true,
+  enableEscapeKey: true,
+});
+
+$('#videoLink2').magnificPopup({
+  type:'inline',
+  midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  closeBtnInside: false,
+  showCloseBtn: true,
+  enableEscapeKey: true,
+});
